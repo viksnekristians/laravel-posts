b6918bebf6f0572e945a16f03fe6e557486ecbb2
Kristians Viksne <kristians.viksne@scandiweb.com>
2023-08-01 12:13:12 +0300

    updated entrypoint file and added command to fix storage link problem

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -19,6 +19,7 @@ if [ "$role" = "app" ]; then
     php artisan cache:clear
     php artisan config:clear
     php artisan route:clear
+    php artisan storage:link
     php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
     exec docker-php-entrypoint "$@"
 fi
